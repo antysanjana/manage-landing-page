1664188dd6ba42a92b7396eca0e469f541bdad3a
Sanjana Anty <sanjana.anty@gmail.com>
2024-08-13 11:34:00 +0600

    fixed nav bar

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")
-    ? navToggle.setAttribute("area-expended", false)
-    : navToggle.setAttribute("area-expended", true);
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
 
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
